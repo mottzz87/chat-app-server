b883c081f1d0c294a9b9d44d374c169f2ceded44
mottzz87 <hualuna87@126.com>
2024-05-31 03:05:00 +0800

    feat: build

--- src/websocket/websocket.gateway.ts
+++ src/websocket/websocket.gateway.ts
@@ -17,11 +17,27 @@ export class WebsocketGateway {
   @WebSocketServer()
   server: Server;
 
+  private clients: Map<string, Socket> = new Map();
+
   @SubscribeMessage('msgToServer')
   async handleMessage(client: any, payload: CreateMessageDto) {
     await this.messageService.create(payload)
-    this.server.emit('msgToClient', payload);
-    // this.server.to(String(payload.receiverId)).emit('msgToClient', payload);
+    // this.server.emit('msgToClient', payload);
+    // console.log(this.server.sockets)
+    // this.server.in(String(payload.receiverId)).emit('msgToClient', payload);
+
+    const { receiverId, content } = payload;
+    if (receiverId && this.clients.has(client.id)) {
+      const recipientSocket = this.clients.get(client.id);
+      recipientSocket.emit('msgToClient', payload);
+    } else {
+      console.log(`Recipient ${receiverId} not found`);
+    }
+  }
+  @SubscribeMessage('subscribeAll')
+  async handleMessageList(client: any, payload: CreateMessageDto) {
+    // this.server.emit('loadUsersMessageToClient', payload);
+    return this.websocketService.findAll()
   }
 
   afterInit(server: Server) {
@@ -29,11 +45,13 @@ export class WebsocketGateway {
   }
 
   handleConnection(client: Socket, ...args: any[]) {
-    console.log(`Client disconnected: ${client.id}`);
+    console.log(`Client connected: ${client.id}`);
+    this.clients.set(client.id, client);
   }
 
   handleDisconnect(client: Socket) {
-    console.log(`Client connected: ${client.id}`);
+    console.log(`Client disconnected: ${client.id}`);
+    this.clients.delete(client.id);
   }
 
   @SubscribeMessage('createWebsocket')
